4bf4e2c9caef7cef1ad8d0d6219b9fe531bb8f3f
tiito31 <52558415+tiito31@users.noreply.github.com>
2020-06-28 15:03:07 +0700

    Change select data in Forms/ams_ix

--- forms/ams_ix.php
+++ forms/ams_ix.php
@@ -44,7 +44,7 @@
                 $data = mysqli_query($koneksi,'select community from ams_ix order by community DESC limit 1');
                 while($d = mysqli_fetch_array($data)){
                 ?>
-                <form action="../actions/action_amx_ix.php" method="POST" style="text-align: center;">
+                <form action="../actions/ams_ix.php" method="POST" style="text-align: center;">
                     <div class="form-group">
                         <div class="row">
                             <div class="col">
