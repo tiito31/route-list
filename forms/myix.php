b00495b4419e82a68e07828853c60dc1bbaa7560
tiito31 <52558415+tiito31@users.noreply.github.com>
2020-06-28 15:01:48 +0700

    Change select data in Forms/myix

--- forms/myix.php
+++ forms/myix.php
@@ -41,7 +41,7 @@
             <div class="card-body">
                 <?php
                 include '../conn/connection.php';
-                $data = mysqli_query($koneksi,'select community from ams_ix order by community DESC limit 1');
+                $data = mysqli_query($koneksi,'select community from myix order by community DESC limit 1');
                 while($d = mysqli_fetch_array($data)){
                 ?>
                 <form action="../actions/action_myix.php" method="POST" style="text-align: center;">
