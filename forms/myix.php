315cc4823cc4e1d44caf40a7c97e8bd8ed8c5e77
tiito31 <52558415+tiito31@users.noreply.github.com>
2020-06-28 15:03:28 +0700

    Change select data in Forms/myix

--- forms/myix.php
+++ forms/myix.php
@@ -44,7 +44,7 @@
                 $data = mysqli_query($koneksi,'select community from myix order by community DESC limit 1');
                 while($d = mysqli_fetch_array($data)){
                 ?>
-                <form action="../actions/action_myix.php" method="POST" style="text-align: center;">
+                <form action="../actions/myix.php" method="POST" style="text-align: center;">
                     <div class="form-group">
                         <div class="row">
                             <div class="col">
